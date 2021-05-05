0888224ef8501d02480e9cfeb5c896ea11787ec3
Martina <martinaberraz@gmail.com>
2021-05-05 16:07:10 -0300

    Cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Prenda.create! descripcion:"remera de algodon", tipo:1, cprimario:2, csecundario:3, categoria:2
+Prenda.create! descripcion:"pantalon de jean", tipo:2, cprimario:0, csecundario:1, categoria:0
+Prenda.create! descripcion:"Camisa larga", tipo:4, cprimario:1, csecundario:2, categoria:1
+Prenda.create! descripcion:"Zapatillas converse", tipo:3, cprimario:1, csecundario:1, categoria:2
+Prenda.create! descripcion:"Pantalon de vestir", tipo:1, cprimario:2, csecundario:3, categoria:1
+Prenda.create! descripcion:"Remera con flores", tipo:2, cprimario:0, csecundario:1, categoria:2
+Prenda.create! descripcion:"Bufanda lisa", tipo:1, cprimario:2, csecundario:2, categoria:1
+Prenda.create! descripcion:"Anteojos de sol", tipo:3, cprimario:1, csecundario:1, categoria:2
