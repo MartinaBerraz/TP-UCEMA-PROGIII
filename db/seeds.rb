0b2975500fd3d146ea14cac29bd7d2a72b12f439
Martina <martinaberraz@gmail.com>
2021-06-12 19:59:35 -0300

    ajustes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-user1 = User.create! nombre: "user1", email: "mb@gmail.com", password: "user123" , admin:false
+user = User.create! nombre: "user", email: "u@gmail.com", password: "user12345" , admin:false
 user2 = User.create! nombre: "Admin1", email: "skiba_florencia@yahoo.com.ar", password: "user12345", admin:true
 user3 = User.create! nombre: "Admin4", email: "mateorial22@gmail.com", password: "user12345", admin:true
 user4 = User.create! nombre: "Admin3", email: "martuberraz@hotmail.com", password: "user12345", admin:true
 
-vieja = Guardarropa.create! nombre: "Ropa vieja"
-favorita = Guardarropa.create! nombre: "Ropa favorita"
-trabajo = Guardarropa.create! nombre: "Ropa de trabajo"
+vieja = Guardarropa.create! nombre: "Ropa vieja", user_id: user.id
+favorita = Guardarropa.create! nombre: "Ropa favorita", user_id: user.id
+trabajo = Guardarropa.create! nombre: "Ropa de trabajo", user_id: user.id
 
-vieja.user_id = user1.id
-favorita.user_id = user1.id
-trabajo.user_id = user1.id
 
-remera_para_dormir = Prenda.create! descripcion:"remera de algodon", tipo:1, cprimario:"rosa", csecundario:"rosa", categoria:0
+remera_para_dormir = Prenda.create! descripcion:"remera de algodon", tipo:1, cprimario:"rosa", csecundario:"rosa", categoria:0, user_id:user.id
 
-remera_para_dormir.user_id = user1.id
 vieja.prendas << remera_para_dormir
 favorita.prendas << remera_para_dormir
 
